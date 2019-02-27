ecad4a7af6508ffcee5c530fe784556cdb9bf83e
Frank Martinez <frankie.martinez@gmail.com>
2019-02-27 12:03:22 -0800

    scaffolding complete.

---
+++ friends/src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './css/index.css';
+import axios from 'axios';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      friends:[]
+    }
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/friends')
+      .then(response => {
+        console.log(response);
+        this.setState({ friends: response.data })
+      })
+      .catch(error => {});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
